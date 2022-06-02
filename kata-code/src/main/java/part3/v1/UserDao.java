c2c6682b936fec83ad35f818cbd5f278a7baa39d
holee <ho.sol.lee@gmail.com>
2022-06-03 01:15:41 +0900

    feat: Part 3, try/catch/finally문으로 작성된 복잡한 기본 UserDao

---
+++ kata-code/src/main/java/part3/v1/UserDao.java
@@ -0,0 +1,85 @@
+package part3.v1;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UserDao {
+
+    private DataSource dataSource;
+
+    public void setDataSource(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public void deleteAll() throws SQLException {
+        Connection connection = dataSource.getConnection();
+
+        // prepareStatement를 호출하다가 예외가 발생하는 경우 statement와 connection이 정상적으로 닫히지 않는다.
+        PreparedStatement ps = connection.prepareStatement("delete from users");
+        ps.executeUpdate();
+
+        ps.close();
+        connection.close();
+    }
+
+    public void deleteAllWithTryCatch() throws SQLException {
+        Connection conn = null;
+        PreparedStatement ps = null;
+
+        try {
+            conn = dataSource.getConnection();
+            ps = conn.prepareStatement("delete from users");
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw e;
+        } finally {
+            if( ps != null){
+                try {
+                    ps.close();
+                } catch (SQLException e){}
+            }
+            if(conn != null){
+                try {
+                    conn.close();
+                } catch (SQLException e){}
+            }
+        }
+    }
+
+    public int getCount() throws SQLException {
+        Connection conn = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        try {
+            conn = dataSource.getConnection();
+
+            ps = conn.prepareStatement("select count(*) from users");
+
+            rs = ps.executeQuery();
+            rs.next();
+            return rs.getInt(1);
+        } catch (SQLException e){
+            throw e;
+        } finally {
+            if( rs != null){
+                try {
+                    rs.close();
+                } catch (SQLException e){}
+            }
+            if( ps != null){
+                try {
+                    ps.close();
+                } catch (SQLException e){}
+            }
+            if(conn != null){
+                try {
+                    conn.close();
+                } catch (SQLException e){}
+            }
+        }
+    }
+}
